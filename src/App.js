4b789df1286421e1d4498d5e7971e73073f4b20c
박건규 <geongyu09@gmail.com>
2023-10-29 16:14:37 +0900

    add code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRecoilState } from "recoil";
+import { textState } from ".";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useRecoilState(textState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <span>{text}</span>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </section>
   );
 }
 
